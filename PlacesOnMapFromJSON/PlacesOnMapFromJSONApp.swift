c66a895d0ad75721c1839e3834e16f4476d0e4ad
Keyhou <31210145+Keyhou@users.noreply.github.com>
2022-09-26 09:48:27 +0200

    Initial Commit

---
+++ PlacesOnMapFromJSON/PlacesOnMapFromJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesOnMapFromJSONApp.swift
+//  PlacesOnMapFromJSON
+//
+//  Created by Keyhan Mortezaeifar on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PlacesOnMapFromJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
